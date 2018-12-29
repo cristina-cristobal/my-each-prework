ed71f6c298ac1152ce72a97e0aeff6c0d59b3cb6
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-29 15:11:19 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each(array)
+  i = 0
+  while i < array.size
+    block_given? # optional
+    yield(array[i]) # if block, then yield to block # also need to pass argument(s)
+    #to yield, otherwise block is operating on nil
+    i += 1
+  end
+array
